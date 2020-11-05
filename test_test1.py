9e52da73327007aefac76e3d1313c14fbd376df6
stuznik <stanley.tuznik@dcsg.com>
2020-11-05 13:50:06 -0500

    test bq

--- test_test1.py
+++ test_test1.py
@@ -1,3 +1,21 @@
+import pytest
+import ds
+import pandas as pd
+from datetime import datetime
+
+from ds import bigquery as bq 
+import gcsfs
+import google.cloud.bigquery as bigquery
+from google.cloud import bigquery_storage_v1
+from typing import Dict, List, Mapping, Tuple, Union
+
+BQ_CLIENT = bigquery.Client(project="d-ml-8576245384")
+GCS = gcsfs.GCSFileSystem(project="d-ml-8576245384")
+fast_bq_client = bigquery_storage_v1.BigQueryReadClient()
+
+
+
+
 def test_mock():
   assert True
 
@@ -10,3 +28,15 @@ def test_import_dsgdatascience():
 def test_will_fail():
   print("This will fail")
   raise Exception("oops")
+
+def test_bq_table_to_df():
+  data = {"date_id": [20200104.], "date_key": [20200104.]}
+    expected = pd.DataFrame(data)
+    
+    result = bq.bq_table_to_df(table="d-ml-8576245384.datascience_utils.gcs_to_bq_test")
+    result = result.iloc[[4]].reset_index(drop=True)
+
+    if pd.testing.assert_frame_equal(left=expected, right=result, check_dtype=False):
+        pass
+    else:
+      raise Execption("you fail")
